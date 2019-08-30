9d61249e3f2d121c8af065028a824fac3a48ce63
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-30 08:43:37 -0700

    Agregar archivos de proyecto.

---
+++ ContextoEstatico/Matematicas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextoEstatico
+{
+    class Matematicas
+    {
+        public static double sumar(double sumando1, double sumando2)
+        {
+            return sumando1 + sumando2;
+        }
+
+        public const double PI = 3.1416;
+
+        public static double areaCirculo(double radio)
+        {
+            return (radio * radio) * PI;
+        }
+
+    }
+}
---
+++ ContextoEstatico/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextoEstatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            double sumando1 = 10;
+            double sumando2 = 43;
+            double resultado = Matematicas.sumar(sumando1, sumando2);
+
+            Console.WriteLine(sumando1.ToString() + " + " + sumando2.ToString() + " = " + resultado.ToString());
+
+            Console.WriteLine("Pi: " + Matematicas.PI);
+
+            double radio = 4.7;
+            Console.WriteLine("Radio: " + radio.ToString() +",  Area = " + Matematicas.areaCirculo(radio));
+
+            Console.ReadLine();
+        }
+    }
+}
